298d8cf6cd0d1f038ab9f390481e61869a7277f9
lwto <linwathanoo227@gmail.com>
2021-08-29 10:44:45 +0630

    fom control

---
+++ javascript/main.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('form')
+const email = document.getElementById('email')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+})
+
+function checkInputs() {
+    const emailValue = email.value.trim();
+    if (emailValue === '') {
+        setErrorFor(email, 'Please provide a valid email')
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Please provide a valid email');
+    } else {
+        alert('success');
+        location.reload();
+    }
+
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+
+    small.innerText = message;
+
+    formControl.className = 'form-control error'
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
